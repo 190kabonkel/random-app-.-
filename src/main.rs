0c453007b9d845897727658fd1dcab7eb50e2dcf
190kabonkel <kabonkel@proton.me>
2026-06-17 09:23:23 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
 fn main() {
     println!("Hello, world!");
-    println!("hey bertus foot print");
+    println!("vinci software wants to expand into rust");
+    println!("cargo build to compile into a executable rust project this wil be verry slow")
+    println!("cargo run wil run an exacutable rust project")
+    println!("cargo build --version wil show the version of a project")
+    println!("rustc main.rs  is to comile and rust file called main into an exe")
 }
